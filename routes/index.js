37fac5e3e779aeab6643af7a697676e8691524e6
Thibaut <Thibaut@google.fr>
2020-06-10 01:59:12 +0200

    _

--- routes/index.js
+++ routes/index.js
@@ -4,9 +4,26 @@ var tesseract = require('node-tesseract-ocr');
 //const tesseract = require("node-tesseract")
  
 
+
+
+
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
  
+
+  const csvFilePath='./RelevéBancaire.csv'
+  const csv=require('csvtojson')
+  csv()
+  .fromFile(csvFilePath)
+  .then((jsonObj)=>{
+    console.log(jsonObj);
+  })
+ 
+  // Async / await usage
+  const jsonArray= csv().fromFile(csvFilePath);
   
   const config = {
     lang: "eng",
@@ -18,13 +35,39 @@ router.get('/', function(req, res, next) {
     .then(text => {
       result = text;
 
-      console.log(text);
+      //console.log(text);
 
       const words = text.split(' ');
       last = 'start';
       value = 0;
       words.forEach(element => {
         if (last == 'Facture')
+          value = element.replace(new RegExp("[^(0-9\.)]", "g"), '');
+        last = element;
+        //console.log(element + " " + last);
+      });
+      /*
+      console.log(" ");
+      console.log("-------OCR RESULT-------");      
+      console.log("Prix indiquer sur le ticket de caisse: "+ value);
+      console.log("------------------------");
+
+        */
+      //console.log(error.message)
+    })
+
+
+  tesseract.recognize("image4.jpg", config)
+    .then(text => {
+      result = text;
+
+      //console.log(text);
+
+      const words = text.split(' ');
+      last = 'start';
+      value = 0;
+      words.forEach(element => {
+        if (last == 'PAYER')
           value = element.replace(new RegExp("[^(0-9\.)]", "g"), '');
         last = element;
         //console.log(element + " " + last);
@@ -36,8 +79,34 @@ router.get('/', function(req, res, next) {
       console.log("------------------------");
 
 
-      console.log(error.message)
+      //console.log(error.message)
     })
+    
+    tesseract.recognize("impot2.png", config)
+    .then(text => {
+      result = text;
+
+      console.log(text);
+
+      const words = text.split(' ');
+      last = 'start';
+      value = 0;
+      words.forEach(element => {
+        if (last == '12')
+          value = element.replace(new RegExp("[^(0-9\.)]", "g"), '');
+        last = element;
+        //console.log(element + " " + last);
+      });
+
+      console.log(" ");
+      console.log("-------OCR RESULT-------");      
+      console.log("Prix indiquer sur le ticket de caisse: "+ value);
+      console.log("------------------------");
+
+
+      //console.log(error.message)
+    })
+
 
   res.render('index', { title: "Resultat dans la console" });
 });
